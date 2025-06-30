ffeb7e474fc3dda6b191ba17138b9dbcc30ec4e2
Snigdha Datta <116970908+SnigdhaDatta@users.noreply.github.com>
2025-07-01 00:37:05 +0530

    changed the order of how synonyms will be displayed, changed favicon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,17 @@
-export default function Footer(){
-    return(
-        <div className="w-full flex justify-center pt-6">
-            <p className="font-mono font-bold text-center">
-            Made with 💗 by 
-            <a 
-            className="hover:text-purple-500 pl-1" 
-            href="https://github.com/SnigdhaDatta" 
-            target="_blank" 
-            rel="noopener noreferrer"
-            >
-            SnigdhaDatta
-            </a>
-        </p>
-        </div>
-
-    )
+export default function Footer() {
+  return (
+    <div className="w-full flex justify-center pt-6">
+      <p className="font-mono font-bold text-center">
+        Made with 💗 by
+        <a
+          className="hover:text-purple-500 pl-1"
+          href="https://github.com/SnigdhaDatta"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          SnigdhaDatta
+        </a>
+      </p>
+    </div>
+  );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,27 +1,28 @@
 export default function SearchBar(props) {
-    const {word , setWord , setShowDisplayCard}=props
-    return(
-        <div className="relative pl-4 pr-2">
-            <input
-            type="text" 
-            value={word}
-            onChange={(e)=>{setWord(e.target.value)
-                console.log("new word has been setted")
-                }
-            }
-            name="word"
-            placeholder="Type a Word" 
-            className="bg-gray-200 w-full h-12 rounded-md p-4 shadow-2xl shadow-purple-500"
-            />
-            <i className="fa-solid fa-magnifying-glass 
+  const { word, setWord, setShowDisplayCard } = props;
+  return (
+    <div className="relative pl-4 pr-2">
+      <input
+        type="text"
+        value={word}
+        onChange={(e) => {
+          setWord(e.target.value);
+          console.log("new word has been setted");
+        }}
+        name="word"
+        placeholder="Type a Word"
+        className="bg-gray-200 w-full h-12 rounded-md p-4 shadow-2xl shadow-purple-500"
+      />
+      <i
+        className="fa-solid fa-magnifying-glass 
                text-2xl 
                absolute right-7 top-1/2 
                transform -translate-y-1/2 
                text-gray-600 hover:text-purple-600 
                hover:scale-90 transition duration-300 
-               cursor-pointer" onClick={()=>setShowDisplayCard(true)}></i>
-        </div>
-        
-    )
-    
+               cursor-pointer"
+        onClick={() => setShowDisplayCard(true)}
+      ></i>
+    </div>
+  );
 }
