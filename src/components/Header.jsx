3632419281492b18687c1828490b586da66fddee
Snigdha Datta <116970908+SnigdhaDatta@users.noreply.github.com>
2025-06-30 23:30:24 +0530

    shifting theme icon to right

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@
 export default function Header(props){
     //const {setShowDisplayCard}=props
     return(
-        <div className="relative flex pl-2 pr-8 pb-5 pt-10">
+        <div className="relative flex items-center pl-2 pr-8 pb-5 pt-10">
             <i className="fa-solid fa-2x fa-book hover:scale-90 transition duration-300 cursor-pointer" onClick={()=>window.location.reload()}></i>  
              <p className="pl-1.5 pt-1.5 font-mono">Vocabulex</p>
-             <i className="fa-regular fa-1.5x fa-moon  text-2xl hover:scale-90 transition duration-300 cursor-pointer relative md:left-10/12 left-8/12 mr-10"></i>   
+             <i className="fa-regular fa-1.5x fa-moon  text-2xl hover:scale-90 transition duration-300 cursor-pointer relative md:left-10/12 left-8/12 mr-12"></i>   
                
         </div>
     )
