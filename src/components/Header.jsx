5fb22f3506f247a9e21dc6868bc03e9821b52919
Snigdha Datta <116970908+SnigdhaDatta@users.noreply.github.com>
2025-07-01 00:39:37 +0530

    Moon icon positioned by @deepsoumya617

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 // import DisplayCard from "./DisplayCard";
-export default function Header(props){
-    //const {setShowDisplayCard}=props
-    return(
-        <div className="relative flex items-center pl-2 pr-8 pb-5 pt-10">
-            <i className="fa-solid fa-2x fa-book hover:scale-90 transition duration-300 cursor-pointer" onClick={()=>window.location.reload()}></i>  
-             <p className="pl-1.5 pt-1.5 font-mono">Vocabulex</p>
-             <i className="fa-regular fa-1.5x fa-moon  text-2xl hover:scale-90 transition duration-300 cursor-pointer relative md:left-10/12 left-8/12 mr-12"></i>   
-               
-        </div>
-    )
+export default function Header(props) {
+  //const {setShowDisplayCard}=props
+  return (
+    <div className="mt-7 mb-5 flex items-center justify-between px-5">
+      <div className="flex items-center gap-1">
+        <i
+          className="fa-solid fa-2x fa-book cursor-pointer transition duration-300 hover:scale-90"
+          onClick={() => window.location.reload()}
+        />
+        <p className="pt-1.5 pl-1.5 font-mono">Vocabulex</p>
+      </div>
+      <i className="fa-regular fa-1.5x fa-moon cursor-pointer text-2xl transition duration-300 hover:scale-90" />
+    </div>
+  )
 }
