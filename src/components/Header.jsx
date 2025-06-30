142189b2f52a730e94fe39308716d7154822f1d9
Snigdha Datta <116970908+SnigdhaDatta@users.noreply.github.com>
2025-06-30 22:52:07 +0530

    shifting theme icon to right

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header(props){
         <div className="flex pl-2 pr-8 pb-5 pt-10">
             <i className="fa-solid fa-2x fa-book hover:scale-90 transition duration-300 cursor-pointer" onClick={()=>window.location.reload()}></i>  
              <p className="pl-1.5 pt-1.5 font-mono">Vocabulex</p>
-             <i className="fa-regular fa-1.5x fa-moon  text-3xl hover:scale-90 transition duration-300 cursor-pointer relative md:left-10/12 left-8/12 pr-10"></i>   
+             <i className="fa-regular fa-1.5x fa-moon  text-3xl hover:scale-90 transition duration-300 cursor-pointer relative md:left-10/12 left-8/12 mr-10"></i>   
                
         </div>
     )
