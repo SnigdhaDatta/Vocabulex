d25149f61905603236199a44dc070b02137d0714
Snigdha Datta <116970908+SnigdhaDatta@users.noreply.github.com>
2025-06-30 22:21:56 +0530

    Added Web app name beside icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,11 @@
 export default function Header(props){
     //const {setShowDisplayCard}=props
     return(
-        <div className=" flex pl-4 pr-8 pb-5 pt-10">
+        <div className="flex pl-2 pr-8 pb-5 pt-10">
             <i className="fa-solid fa-2x fa-book hover:scale-90 transition duration-300 cursor-pointer" onClick={()=>window.location.reload()}></i>  
-             <i className="fa-regular fa-2x fa-moon  text-3xl hover:scale-90 transition duration-300 cursor-pointer relative left-11/12 pr-10"></i>     
+             <p className="pl-1.5 pt-1.5 font-mono">Vocabulex</p>
+             <i className="fa-regular fa-1.5x fa-moon  text-3xl hover:scale-90 transition duration-300 cursor-pointer relative md:left-10/12 left-8/12 pr-10"></i>   
+               
         </div>
     )
 }
