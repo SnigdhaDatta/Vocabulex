ff630a2817afb060572f43a5f31566fb015ff933
Snigdha Datta <116970908+SnigdhaDatta@users.noreply.github.com>
2025-07-03 11:24:13 +0530

    Formatted the tailwind classes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,28 +1,22 @@
 export default function SearchBar(props) {
-  const { word, setWord, setShowDisplayCard } = props;
+  const { word, setWord, setShowDisplayCard } = props
   return (
-    <div className="relative pl-4 pr-2">
+    <div className="relative pr-2 pl-4">
       <input
         type="text"
         value={word}
         onChange={(e) => {
-          setWord(e.target.value);
-          console.log("new word has been setted");
+          setWord(e.target.value)
+          console.log('new word has been setted')
         }}
         name="word"
         placeholder="Type a Word"
-        className="bg-zinc-200 w-full h-12 rounded-md p-4 shadow-2xl shadow-purple-500 dark:bg-zinc-900 focus:outline-none"
+        className="h-12 w-full rounded-md bg-zinc-200 p-4 shadow-2xl shadow-purple-500 focus:outline-none dark:bg-zinc-900"
       />
       <i
-        className="fa-solid fa-magnifying-glass 
-               text-2xl 
-               absolute right-7 top-1/2 
-               transform -translate-y-1/2 
-               text-gray-600 hover:text-purple-600 
-               hover:scale-90 transition duration-300 
-               cursor-pointer"
+        className="fa-solid fa-magnifying-glass absolute top-1/2 right-7 -translate-y-1/2 transform cursor-pointer text-2xl text-gray-600 transition duration-300 hover:scale-90 hover:text-purple-600"
         onClick={() => setShowDisplayCard(true)}
       ></i>
     </div>
-  );
+  )
 }
